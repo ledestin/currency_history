a80730d38034ea40b6578bd3dc20ea50b9ec0c2b
Dmitry Maksyoma <ledestin@gmail.com>
2019-02-02 00:24:49 +1300

    Rename HistoryController -> RatesController

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,4 +1,4 @@
-class HistoryController < ApplicationController
+class RatesController < ApplicationController
   def index
     latest_rates = Rate.order(:updated_by_api_at).limit(24)
     @usd_to_brl_series = assemble_highchart_sseries(latest_rates, "USDBRL")
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  root to: "history#index"
+  root to: "rates#index"
 end
