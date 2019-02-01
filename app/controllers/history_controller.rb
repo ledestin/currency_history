6cc097f6ce018d0024ef16026dcfa71df78d7122
Dmitry Maksyoma <ledestin@gmail.com>
2019-02-01 22:23:51 +1300

    Refactor: extract #assemble_highchart_sseries

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,8 +1,14 @@
 class HistoryController < ApplicationController
   def index
     latest_rates = Rate.order(:updated_by_api_at).limit(24)
-    @usd_to_brl_series = latest_rates.map do |rate|
-      [rate.updated_by_api_at.to_i * 1000, rate.brl_rates["USDBRL"]]
+    @usd_to_brl_series = assemble_highchart_sseries(latest_rates, "USDBRL")
+  end
+
+  private
+
+  def assemble_highchart_sseries(rates, series_name)
+    rates.map do |rate|
+      [rate.updated_by_api_at.to_i * 1000, rate.brl_rates[series_name]]
     end
   end
 end
