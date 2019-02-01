25b08f41ff05a252bc9e8c66dd2921c318931729
Dmitry Maksyoma <ledestin@gmail.com>
2019-02-01 16:11:43 +1300

    Refactor: extract LATEST_RATES_URL

--- app/services/fetch_latest_rates.rb
+++ app/services/fetch_latest_rates.rb
@@ -1,8 +1,10 @@
 module FetchLatestRates
   SECRET_API_KEY = ENV["APILAYER_SECRET_KEY"]
+  LATEST_RATES_URL =
+    "http://www.apilayer.net/api/live?access_key=#{SECRET_API_KEY}&currencies=USD,EUR,AUD,BRL"
 
   def self.call
-    json = Faraday.get("http://www.apilayer.net/api/live?access_key=#{SECRET_API_KEY}&currencies=USD,EUR,AUD,BRL").body
+    json = Faraday.get(LATEST_RATES_URL).body
     JSON.parse json
   end
 end
