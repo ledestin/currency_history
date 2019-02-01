97c9e56d78f31324f51cdc2816681b8b956f13f5
Dmitry Maksyoma <ledestin@gmail.com>
2019-02-01 20:19:34 +1300

    Refactor: enforce line length

--- app/services/save_latest_rates.rb
+++ app/services/save_latest_rates.rb
@@ -1,7 +1,8 @@
 module SaveLatestRates
   def self.call
     rates = FetchLatestRates.call
-    Rate.create! api_response: rates, updated_by_api_at: rates_updated_by_api_at(rates)
+    Rate.create! api_response: rates,
+      updated_by_api_at: rates_updated_by_api_at(rates)
   end
 
   def self.rates_updated_by_api_at(rates)
